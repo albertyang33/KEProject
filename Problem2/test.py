ad849799e40c2f7852b3dd82236d257fa1b8845d
albertyang33 <cheng-ya14@mails.tsinghua.edu.cn>
2017-07-18 14:38:00 +0000

    update problems

---
+++ Problem2/test.py
@@ -0,0 +1,4 @@
+# You need to write down your own code here 
+# Task: Given any head entity name (e.g. Q30) and relation name (e.g. P6), you need to output the top 10 closest tail entity names.
+# File entity2vec.vec and relation2vec.vec are 50-dimensional entity and relation embeddings.
+# If you use the embeddings learned from Problem 1, you will get extra credits.
